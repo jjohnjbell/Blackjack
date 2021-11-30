d7010061f6e017290844d4ee2ecbc6ba87d35455
jjohnjbell <83473305+jjohnjbell@users.noreply.github.com>
2021-11-30 18:06:27 -0500

    commit on main

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
 window.onload = function () {
-let currentNum = 0
-let sum = 0
-let blackjack = false;
-let randNum = Math.floor(Math.random()*11)
-let sumEl = document.getElementById("sum-el")
-console.log(randNum)
-let boardEl = document.getElementById("board")
-const cardPath = ["images/ace.png","images/2.png","images/3.png",
-                "images/4.png","images/5.png","images/6.png","images/7.png",
-                "images/8.png","images/9.png","images/jack.jpg",
-                "images/queen.png","images/king.jpg"]
-const cards = document.querySelectorAll("image")
-const deck = Array.from(cards)
+    let currentNum = 0
+    let blackjack = false;
+
+    let sumEl = document.getElementById("sum-el")
+    let newGame = document.getElementById("newGameBtn")
+    let resetBut = document.getElementById("resetBtn")
+
+    let boardEl = document.getElementById("board")
+    const cardPath = ["images/ace.png", "images/2.png", "images/3.png",
+        "images/4.png", "images/5.png", "images/6.png", "images/7.png",
+        "images/8.png", "images/9.png", "images/jack.jpg",
+        "images/queen.png", "images/king.jpg"]
 
 
-start()
+    newGame.addEventListener("click", function () {
+            let pic = document.createElement('img')
+            let ran = Math.floor(Math.random() * 11)
+            pic.src = cardPath[ran]
+            boardEl.appendChild(pic)
+            checkWin()
+            currentNum += ran + 1
+            sumEl.textContent = ` Sum:  ${currentNum}`
+    })
 
+    function checkWin(){
+       if (currentNum === 21) {
+            blackjack = true
+            sumEl.textContent = `Congratulations! You Won!`
+        } else {
+            blackjack = false
+            sumEl.textContent = `Sorry! You lost! New Game?`
+        }
+    }
 
-function start(){
-    let pic = document.createElement('img')
-    pic.src = cardPath[randNum]
-    boardEl.appendChild(pic)
-    currentNum = randNum + 1
-    sumEl.textContent +=  currentNum
-}
-
-
-// <script>
-//     var images = ['img/background.png','img/background1.png','img/background2.png','img/background3.png'];
-//     var index = 0;
-
-//     function buildImage() {
-//       var img = document.createElement('img')
-//       img.src = images[index];
-//       document.getElementById('content').appendChild(img);
-//     }
-
-//     function changeImage(){
-//       var img = document.getElementById('content').getElementsByTagName('img')[0]
-//       index++;
-//       index = index % images.length; // This is for if this is the last image then goto first image
-//       img.src = images[index];
-//     }
-// </script>
-
-// <body onload="buildImage();">
-//     <div class="contents" id="content"></div>
-//     <button onclick="changeImage()">NextImage</button>
-// </body>
+    resetBut.addEventListener("click", function () {
+        boardEl.innerHTML = ""
+        currentNum = 0
+        sumEl.textContent = ""
+    })
 
 
 
