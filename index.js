1587e26579b1ab8ac5772a4494f96dee706e2547
Joshua Bell <83473305+jjohnjbell@users.noreply.github.com>
2021-11-30 13:21:15 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+let sum = 0
+let blackjack = false;
+let boardEl = document.querySelectorAll("img")
+const cards = document.querySelectorAll(".cards")
+const deck = Array.from(cards)
+boardEl.innerHTML = deck[2]
+
+
+
+
+
+
+
+
+
+
+
+
+
+} //end of Window onload function
