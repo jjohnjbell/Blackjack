19b924163c1f10671ca4cad97dfbffa1ea0cb160
jjohnjbell <83473305+jjohnjbell@users.noreply.github.com>
2021-12-04 22:14:49 -0500

    commit on dealer

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 window.onload = function () {
-    let total = 0
-    let blackjack = false;
+    let playerTotal = 0
+    let dealerTotal = 0
+    
+    let playerBlackJack = false;
+    let dealerBlackJack = false;
     let gameEnd = false
     let sumEl = document.getElementById("sum-el")
 
     const newGame = document.getElementById("newGameBtn")
     const resetBut = document.getElementById("resetBtn")
-    const boardEl = document.getElementById("board")
+    const playerBoardEl = document.getElementById("player-board")
+    const dealerBoardEl = document.getElementById("dealer-board")
     const cardPath = ["images/ace.png", "images/2.png", "images/3.png",
         "images/4.png", "images/5.png", "images/6.png", "images/7.png",
         "images/8.png", "images/9.png", "images/jack.jpg",
@@ -14,7 +18,7 @@ window.onload = function () {
 
     //Add Event Listener to "New Game" button to render the game
     newGame.addEventListener("click", function () {
-        if (!blackjack && !gameEnd) {
+        if ((!playerBlackJack && !gameEnd) || (!gameEnd && !dealerBlackJack)) {
             play()
 
         }
@@ -22,43 +26,99 @@ window.onload = function () {
         function play() {
             newGame.innerText = "HIT ME"
             //Add new image elements then provide them with random image paths using random index
-            let pic = document.createElement('img')
-            let ran = Math.floor(Math.random() * 11)
-            pic.src = cardPath[ran]
-            if (ran >= 10) {
-                total += ran
+            let playerPic = document.createElement('img')
+            let playerRand = Math.floor(Math.random() * 11)
+            playerPic.src = cardPath[playerRand]
+
+            if (playerRand >= 10) {
+                playerTotal += playerRand
             } else {
-                //Keep track of Total 
-                total += ran + 1
+                //Keep track of playerTotal 
+                playerTotal += playerRand + 1
             }
+
+
+            let dealerPic = document.createElement('img')
+            let dealerRand = Math.floor(Math.random() * 11)
+            dealerPic.src = cardPath[dealerRand]
+
+            if (dealerRand >=10){
+                dealerTotal += dealerRand
+            } else {
+                dealerTotal += dealerRand + 1
+            }
+  
+
             //Append the newly created Image elements to the div sectional
-            boardEl.appendChild(pic)
-            sumEl.textContent = ` Sum:  ${total}`
+            playerBoardEl.appendChild(playerPic)
+            dealerBoardEl.appendChild(dealerPic)
+            sumEl.textContent = ` Dealer Sum: ${dealerTotal}  Player Sum: ${playerTotal}`
+
+
+            // //Logics of Game
+            // if (playerTotal === 21) {
+            //     playerWon()
+
+            // } else if (playerTotal > 21) {
+
+            //     lost()
+
+            // }
+
+
             //Logics of Game
-            if (total === 21) {
-                won()
+            if ((playerTotal === 21 && dealerTotal != 21) || (playerTotal < 21 && dealerTotal>21)) {
+                playerWon(playerTotal)
 
-            } else if (total > 21) {
+            } else if ((dealerTotal === 21 &&  playerTotal != 21) || (dealerTotal < 21 && playerTotal > 21)) {
 
-                lost()
+                playerLost(dealerTotal)
 
+            }else if (dealerTotal > 21 && playerTotal > 21) {
+                draw()
             }
 
+
+
+
         }
 
-        function won() {
+
+        function draw() {
             newGame.style.display = "none"
             resetBut.innerText = "NEW GAME"
-            blackjack = true
             gameEnd = true
-            sumEl.textContent = `CONGRATULATIONS! YOU GOT BLACKJACK! ${total}`
-
+            sumEl.textContent = `DRAW! 
+            Dealer Sum: ${dealerTotal}      Player Sum: ${playerTotal}`
         }
 
-        function lost() {
+
+
+        function playerWon(victory) {
+            if(victory ===21){
+                playerBlackJack = true
+                sumEl.textContent = `CONGRATULATIONS!
+                 YOU GOT BLACKJACK! ${playerTotal}`
+            } else {
+                sumEl.textContent = `CONGRATULATIONS! YOU WON!
+                 Dealer Sum: ${dealerTotal}       Player Sum: ${playerTotal}`
+            }
             newGame.style.display = "none"
             resetBut.innerText = "NEW GAME"
-            sumEl.textContent = `You Lost!  ${total}`
+            gameEnd = true
+        }
+
+        function playerLost(victory) {
+            if(victory === 21){
+                dealerBlackJack=true
+                sumEl.textContent = `You Lost! Dealer Got BlackJack 
+                Dealer Sum: ${dealerTotal}    Player Sum: ${playerTotal}`
+            }else{
+                sumEl.textContent = `You Lost! 
+                Dealer Sum: ${dealerTotal}       Player Sum: ${playerTotal}`
+            }
+            newGame.style.display = "none"
+            resetBut.innerText = "NEW GAME"
             gameEnd = true
 
         }
@@ -68,8 +128,10 @@ window.onload = function () {
     resetBut.addEventListener("click", function () {
         newGame.style.display = "inline"
         resetBut.innerText = "RESET"
-        boardEl.innerHTML = ""
-        total = 0
+        playerBoardEl.innerHTML = ""
+        dealerBoardEl.innerHTML = ""
+        playerTotal = 0
+        dealerTotal = 0
         sumEl.textContent = ""
         gameEnd = false
         blackjack = false
